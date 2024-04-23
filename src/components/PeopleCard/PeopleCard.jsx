77a2e98758c38e0afb64211d8b1fa53e1cdd6ae8
Pallav34 <pallavbag1@gmail.com>
2024-04-23 10:28:57 +0530

    directorPage

--- src/components/PeopleCard/PeopleCard.jsx
+++ src/components/PeopleCard/PeopleCard.jsx
@@ -2,6 +2,7 @@
 import chair from '../../assets/chair.jpg'
 import people from '../../assets/people.jpg'
 import img1 from '../../assets/img1.png';
+import director from '../../assets/profile-pic.png';
 import styles from './PeopleCard.module.css'
 const PeopleCard = () => {
   return (
@@ -15,10 +16,15 @@ const PeopleCard = () => {
       />
       
       <img
-        src={img1}
+        src={director}
         alt="Small Image"
         className={styles.directorimg}
       />
+      <div className={styles.cardname}>
+      <h2 >Gokulnanda Ghorai</h2>
+      <p >Managing Director</p>
+      </div>
+      
     </div>
     <div className={styles.peoplecontainer}>
         <div className={styles.peoplecard}>
